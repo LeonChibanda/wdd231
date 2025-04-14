b3b64375fb812912115be7572c3f1857d7fe1f34
LeonChibanda <leonchibanda@icloud.com>
2025-04-15 01:51:02 +0200

    ...

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -37,3 +37,46 @@ function displaySinceLastVisit() {
 window.onload = (event) => {
     displaySinceLastVisit();
 }
+
+import { displayLocationDetails } from "./discover-model.js";
+
+const areaUrl = 'https://raw.githubusercontent.com/leonchibanda/wdd231/refs/heads/main/chamber/discoverItems.json';
+
+const cards = document.querySelector('#areas');
+
+async function getareaData() {
+    const response = await fetch(areaUrl);
+    const data = await response.json();
+    displayareas(data.areas);
+
+}
+
+getareaData();
+
+function displayareas(areas) {
+    const locationCard = document.querySelector("#areas");
+    locationCard.innerHTML = "";
+
+
+    areas.forEach(area => {
+
+        const areaCard = document.createElement("section");
+        areaCard.classList.add("area-card");
+
+    
+        areaCard.innerHTML = `
+            <img src="${area.imageurl}" alt="${area.name}" loading="lazy" width="300" height="200">
+            <h2>${area.name}</h2>
+            <button class="more-info">Learn More</button>
+        `;
+        
+        const moreInfo = areaCard.querySelector('.more-info');
+        moreInfo.addEventListener("click", () => displayLocationDetails(area));
+
+        locationCard.appendChild(areaCard);
+    });
+
+    
+}
+
+displayareas(areaUrl);
---
+++ chamber/scripts/discovery-modals.js
@@ -0,0 +1,17 @@
+
+export function displayLocationDetails(area) {
+    const dialog = document.querySelector('#location-details');
+
+    dialog.innerHTML = `
+        <button id="closeModal">❌</button>
+        <h2>${area.name}</h2>
+        <p><strong>Address:</strong> ${area.addresses}</p>
+        <p><strong>Description:</strong> ${area.description}</p>
+    `;
+
+    dialog.showModal();
+
+    document.getElementById('closeModal').addEventListener('click', () => {
+        dialog.close();
+    });
+}
